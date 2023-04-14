03284301769450d13869ae6e235db9320a3d7726
Mostafejur Rahman <106027543+mostafejur21@users.noreply.github.com>
2023-04-15 01:10:10 +0600

    Update ostadFirstAssignment.dart

--- ostadFirstAssignment.dart
+++ ostadFirstAssignment.dart
@@ -1,30 +1,33 @@
 void main() {
   List<String> phoneNumber = [
-    '01768131685',
     '+88',
+    '01768131685',
     '01768171985',
     '01768111286',
     '01768131685',
     '01887788771'
   ];
 
-  String plus88 = '';
+  String bdCountryCode = '';
+  //this loop search the +88 in the list and put it in the bdCountryCode variable
 
   for (int i = 0; i < phoneNumber.length; i++) {
     if (phoneNumber[i].contains('+88')) {
-      plus88 = phoneNumber[i];
+      bdCountryCode = phoneNumber[i];
       break;
     }
   }
 
+  //this loop add +88 infront of every number except +88
   for (int i = 0; i < phoneNumber.length; i++) {
-    if (phoneNumber[i] != plus88) {
+    if (phoneNumber[i] != bdCountryCode) {
       phoneNumber[i] = '+88${phoneNumber[i]}';
     }
   }
 
+  //this loop print the list without +88
   for (String number in phoneNumber) {
-    if (number != plus88) {
+    if (number != bdCountryCode) {
       print(number);
     }
   }
